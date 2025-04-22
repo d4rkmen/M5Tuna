a5b4ad00aa9e3a53eeb767267156c4095bf6a2b2
DmytroM <darkmen@i.ua>
2025-04-22 11:18:36 +0300

    Merge bin script added

---
+++ merge_files.cmd
@@ -0,0 +1,1 @@
+esptool --chip esp32s3 merge_bin --output build/M5Apps_full.bin 0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x9000 build/apps_nvs.bin 0xE000 build/ota_data_initial.bin 0x10000 build/M5Tuna.bin
